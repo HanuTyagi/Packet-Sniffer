663c946c800d62f6541f783b4485b75ff29a7537
Bhavishya Tyagi <31559206+HanuTyagi@users.noreply.github.com>
2023-01-24 20:16:01 +0530

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -4,4 +4,4 @@ def packet_callback(packet):
     print(packet.show())
 
 # sniff packets on the eth0 interface
-sniff(iface="eth0", prn=packet_callback, store=0)
+sniff(iface="wlan0", prn=packet_callback, store=0)
