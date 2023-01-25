acb3710e7cba7f833e32e2bf8da931b91dc4b9a5
Bhavishya Tyagi <31559206+HanuTyagi@users.noreply.github.com>
2023-01-25 10:49:01 +0530

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -3,5 +3,4 @@ from scapy.all import *
 def packet_callback(packet):
     print(packet.show())
 
-# sniff packets on the wlan0 interface
-sniff(iface="wlan0", prn=packet_callback, store=0)
+sniff(prn=packet_callback)
